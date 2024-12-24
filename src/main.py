3d870a77c8b405738c8ce8bb6e1b484c58341e45
ADerya <derya.ay26@hotmail.com>
2024-12-24 18:40:26 +0100

    feat(datasets):import datasets and get experiment + add load_dotenv

--- src/main.py
+++ src/main.py
@@ -4,22 +4,51 @@ from dotenv import load_dotenv
 from picsellia import Client
 
 load_dotenv()
+WORKSPACE_NAME = "Picsalex-MLOps"
+DATASET_ID = "0193688e-aa8f-7cbe-9396-bec740a262d0"
+OUTPUT_DIR_DATASET = "./datasets"
 
-# Configuration du client
-organization_name = "Picsalex-MLOps"
-dataset_id = "0193688e-aa8f-7cbe-9396-bec740a262d0"
+# Connexion au client Picsellia
+def connect_to_client():
+    return Client(
+        api_token=os.getenv("PICSELLIA_API_TOKEN"),
+        organization_name=WORKSPACE_NAME
+    )
+# Téléchargement du dataset depuis Picsellia
+def import_datasets(client):
+    dataset = client.get_dataset_version_by_id(DATASET_ID)
+    os.makedirs(OUTPUT_DIR_DATASET, exist_ok=True)
+    dataset.list_assets().download(OUTPUT_DIR_DATASET)
+    print("Datasets importés")
+    return dataset
 
-client = Client(
-    api_token=os.getenv("PICSELLIA_API_TOKEN"), organization_name=organization_name
-)
-dataset = client.get_dataset_version_by_id(dataset_id)
 
-output_dir_dataset = "./datasets"
-os.makedirs(output_dir_dataset, exist_ok=True)
-dataset.list_assets().download(output_dir_dataset)
+def get_experiment(client):
+    # Expérimentation existante
+    project = client.get_project(project_name="Groupe_1")
+    experiment = project.get_experiment(name="experiment-0")
+    print(f"Expérimentation existante récupérée : {experiment.name}")
+    datasets = experiment.list_attached_dataset_versions()
+    print(f"Datasets attachés à l'expérience : {datasets}")
+    '''
+        experiment = project.create_experiment(
+            name="experiment-1",
+            description="base experiment",
+        )
 
-project = client.get_project(project_name="Groupe_1")
-experiment = project.create_experiment(
-    name="experiment-0",
-    description="base experiment",
-)
+        experiment.attach_dataset(
+            name="⭐️ cnam_product_2024",
+            dataset_version=dataset,
+        )
+        print(f"Création nouvelle expérimentation : {experiment.name}")
+        '''
+
+def main():
+    # --- PARTIE 1 : Importer les images et les annotations ---
+    client = connect_to_client()
+    dataset = import_datasets(client)
+    get_experiment(client)
+
+
+if __name__ == "__main__":
+    main()
