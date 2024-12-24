7aa8bff809d5236837b3c6e379b8b01dba6de918
ADerya <derya.ay26@hotmail.com>
2024-12-24 18:36:00 +0100

    feat(client):download datasets

--- src/main.py
+++ src/main.py
@@ -12,3 +12,8 @@ dataset_id = "0193688e-aa8f-7cbe-9396-bec740a262d0"
 client = Client(
     api_token=os.getenv("PICSELLIA_API_TOKEN"), organization_name=organization_name
 )
+dataset = client.get_dataset_version_by_id(dataset_id)
+
+output_dir_dataset = "./datasets"
+os.makedirs(output_dir_dataset, exist_ok=True)
+dataset.list_assets().download(output_dir_dataset)
