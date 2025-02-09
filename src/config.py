e0dcdd65cfd1c02657bf07849c1a59d57a2ea8e9
VakrSb <paulchopinet@gmail.com>
2025-02-09 18:02:59 +0100

    config.py

---
+++ src/config.py
@@ -0,0 +1,16 @@
+WORKSPACE_NAME = "Picsalex-MLOps"
+DATASET_ID = "0193688e-aa8f-7cbe-9396-bec740a262d0"
+DATASET_NAME = "⭐️ cnam_product_2024"
+
+PROJECT_ID = "0193641e-53c8-7928-887c-4be047938648"
+PROJECT_NAME = "Groupe_1"
+
+MODEL_NAME = "Groupe_1"
+
+EXPERIMENT_NAME = "experiment-0"
+
+##
+
+YOLO_MODEL = "yolo11n.pt"
+
+
