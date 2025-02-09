ad857730802c6d7fcc452cbc7bfefc1b0a93bdf8
VakrSb <paulchopinet@gmail.com>
2025-02-09 19:09:41 +0100

    get model from latest experiment

--- src/inference.py
+++ src/inference.py
@@ -1,5 +1,7 @@
 import argparse
 import os
+import glob
+import shutil
 from enum import Enum
 
 from dotenv import load_dotenv
@@ -7,8 +9,13 @@ from ultralytics import YOLO
 
 from picsellia_resources.picsellia_client import PicselliaClient
 
+import zipfile
+
 load_dotenv()
 
+MODEL_NAME="model-latest"
+MODEL_DIR_PATH = "./model"
+DEVICE="cuda"
 
 class InferenceMode(Enum):
     WEBCAM = 0
@@ -19,23 +26,35 @@ class InferenceMode(Enum):
 def get_model():
     client = PicselliaClient()
     latest_version = client.get_latest_version()
-    file = latest_version.get_file("best")
-    file.download()
-    return YOLO("best.pt")
+    model_file = latest_version.get_file(MODEL_NAME)
+    model_file.download(MODEL_DIR_PATH)
+    model_filename = os.path.join(MODEL_DIR_PATH, model_file.filename)
+    with zipfile.ZipFile(model_filename, "r") as zip_ref:
+        zip_ref.extractall(MODEL_DIR_PATH)
+    pt_files = glob.glob(f"{MODEL_DIR_PATH}/**/*.pt", recursive=True)
+    yolo_model = YOLO(pt_files[0])
+    # delete all dirs and files in MODEL_DIR_PATH
+    for item in os.listdir(MODEL_DIR_PATH):
+        if os.path.isfile(os.path.join(MODEL_DIR_PATH, item)):
+            os.remove(os.path.join(MODEL_DIR_PATH, item))
+        else:
+            shutil.rmtree(os.path.join(MODEL_DIR_PATH, item))
+    return yolo_model
 
 
 def start_inference(mode: InferenceMode, source_path: str = None) -> None:
     inference_model = get_model()
 
     if mode == InferenceMode.WEBCAM:
-        inference_model(0, device="cuda", show=True)
+        inference_model(0, device=DEVICE, show=True)
     elif mode in [InferenceMode.IMAGE, InferenceMode.VIDEO]:
         if not os.path.exists(source_path):
             print(f"Erreur: Le fichier {source_path} n'existe pas.")
             return
-        result = inference_model(source_path, device="cuda", show=True)
+        results = inference_model(source_path, device=DEVICE, show=True)
         if mode == InferenceMode.IMAGE:
-            result[0].show()
+            for result in results:
+                result.show()
 
 
 if __name__ == "__main__":
